d9b17427281b66073b91767408c23438dfb69c91
ssranked <gorgulenkozxc@gmail.com>
2024-07-18 19:14:07 +0200

    chore: removed unused code

--- extension/background.js
+++ extension/background.js
@@ -1,29 +1,10 @@
 const secret = 'Bafee645a7f9000637f1545302e1e61148bd0e782a94e393f8a79364c76320547'
 
-chrome.webRequest.onHeadersReceived.addListener(
-	(details) => {
-		for (let i = 0; i < details.responseHeaders.length; i++) {
-			if (details.responseHeaders[i].name.toLowerCase() === 'content-security-policy') {
-				details.responseHeaders.splice(i, 1)
-				break
-			}
-		}
-		return { responseHeaders: details.responseHeaders }
-	},
-	{
-		urls: ['*://page.kakao.com/*'],
-		types: ['main_frame', 'sub_frame'],
-	},
-	['responseHeaders']
-)
-
 chrome.action.onClicked.addListener((tab) => {
-	if (tab.url.includes('page.kakao.com')) {
-		chrome.scripting.executeScript({
-			target: { tabId: tab.id },
-			files: ['content.js'],
-		})
-	}
+	chrome.scripting.executeScript({
+		target: { tabId: tab.id },
+		files: ['content.js'],
+	})
 })
 
 chrome.runtime.onMessage.addListener((message, _, sendResponse) => {
